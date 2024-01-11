2ba6eab203699ebc8fc8f7ecc16d30d9a405dba1
Sithael <chojnacki.jamie@gmail.com>
2024-01-11 11:02:12 +0100

    Create project template

---
+++ activate_env.sh
@@ -0,0 +1,9 @@
+DIR=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+ROOT=$DIR
+export ROOT
+
+poetry lock
+poetry install
+source $(poetry env info --path)/bin/activate
+pre-commit install
+echo "Lex Vector activated ..."
